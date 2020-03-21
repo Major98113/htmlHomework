4b8416d6830c213b0cfee3039f674f4009801ca7
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2020-03-21 15:48:14 +0300

    Finished div painting maybe

--- JS/4/4.2/div/index.js
+++ JS/4/4.2/div/index.js
@@ -1,22 +1,30 @@
 const drawField = document.getElementById("draw_field");
+const summitButton = document.getElementsByClassName("submit")[0];
+const clearButton = document.getElementsByClassName("clear")[0];
+const paramX = document.getElementsByClassName("param_x")[0];
+const paramY = document.getElementsByClassName("param_y")[0];
 
-
-function drawEllipse(a, b) { //0.2
+function drawEllipse(a, b) {
     for ( let t = 0; t <= 2 * Math.PI; t += 0.01 ){
+        const point = document.createElement("DIV");
         let x = a * Math.cos(t);
         let y = b * Math.sin(t);
 
-        const point = document.createElement("DIV");
         point.classList.add("point");
         point.style.left = `${250 + x}px`;
         point.style.top = `${250 + y}px`;
-        //console.log(x, y);
-        if (x==-0.5972445280843733  ){
-            console.log(x, y);
-            point.style.width = `${250 + y}px`;
-        }
         drawField.appendChild(point);
     }
-};
+}
 
-drawEllipse(250, 250);
+function clearField() {
+    drawField.innerHTML = "";
+}
+
+summitButton.addEventListener("click", function (event) {
+    drawEllipse(Number(paramX.value), Number(paramY.value));
+});
+
+clearButton.addEventListener("click", clearField);
+
+drawEllipse(200, 200);
